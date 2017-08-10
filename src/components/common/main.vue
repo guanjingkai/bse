cc5a98c061ffd014f7828b43298843ce20033fda
雷丘 <188869727@qq.com>
2017-08-10 19:01:28 +0800

    first commit

---
+++ src/components/common/main.vue
@@ -0,0 +1,47 @@
+<template>
+<div>
+<Row>
+    <Col span='6'>
+      <ModelItem iconfont="MM" font="物料管理"></ModelItem>
+    </Col>
+    <Col span='6'>
+      <ModelItem iconfont="SD" font="分销管理"></ModelItem>
+    </Col>
+    <Col span='6'>
+      <ModelItem iconfont="AM" font="资产管理"></ModelItem>
+    </Col>
+    <Col span='6'>
+      <ModelItem iconfont="CRM" font="客户管理"></ModelItem>
+    </Col>
+</Row>
+<Row>
+    <Col span='6'>
+      <ModelItem iconfont="FICO" font="财务管理"></ModelItem>
+    </Col>
+    <Col span='6'>
+      <ModelItem iconfont="FW" font="工作流"></ModelItem>
+    </Col>
+    <Col span='6'>
+      <ModelItem iconfont="DSP" font="客户管理"></ModelItem>
+    </Col>
+    <Col span='6'>
+      <ModelItem iconfont="BASIC" font="基础设置"></ModelItem>
+    </Col>
+</Row>
+</div>
+</template>
+
+<script>
+import ModelItem from './model'
+export default {
+  name: 'main',
+  components:{
+    'ModelItem':ModelItem
+  },
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
---
+++ src/components/common/model.vue
@@ -0,0 +1,28 @@
+<template>
+<Row>
+    <div class="model">
+      <div>{{iconfont}}</div>
+      <div>物料管理</div>
+    </div>
+</Row>
+</template>
+
+<script>
+export default {
+  name: 'main',
+  props:{
+    iconfont: {
+                type: String
+    },
+    font: {
+                type: String
+    }
+  },
+  data () {
+    return {
+      iconfont:this.$props.iconfont,
+      font:this.$props.font
+    }
+  }
+}
+</script>
