049f443d849fa7da03f0c749eb69e4bf1a94a6d9
关静凯 <188869727@qq.com>
2017-10-12 02:31:28 +0800

    dev

--- src/components/mm/createMaterial.vue
+++ src/components/mm/createMaterial.vue
@@ -8,13 +8,23 @@
     </Tab-pane>
     <Tab-pane label="分类" name="name2">分类</Tab-pane>
     <Tab-pane label="销售/分销" name="name3">销售/分销</Tab-pane>
-    <Tab-pane label="采购" name="name4">采购</Tab-pane>
+    <Tab-pane label="采购" name="name4">
+      <CommonForm :model="createMaterialPurchase"></CommonForm>
+    </Tab-pane>
     <Tab-pane label="需求计划" name="name5">需求计划</Tab-pane>
     <Tab-pane label="存储数据" name="name6">
        <CommonForm :model="createMaterialPlant"></CommonForm>
     </Tab-pane>
     <Tab-pane label="预测数据" name="name7">预测数据</Tab-pane>
-    <Tab-pane label="会计财务" name="name8">预测数据</Tab-pane>
+    <Tab-pane label="会计财务" name="name8">
+      <Tabs type="card">
+        <TabPane label="本期数据">
+          <CommonForm :model="createMaterialPlant"></CommonForm>
+        </TabPane>
+        <TabPane label="1709期">标签二的内容</TabPane>
+        <TabPane label="1708期">标签三的内容</TabPane>
+    </Tabs>
+    </Tab-pane>
   </Tabs>
 </template>
 <script>
@@ -22,12 +32,14 @@
   import createMaterialInit from './mapper/createMaterial_init'
   import createMaterialBasic from './mapper/createMaterial_basic'
   import createMaterialPlant from './mapper/createMaterial_plant'
+  import createMaterialPurchase from './mapper/createMaterial_purchase'
   export default {
     data(){
       return{
         createMaterialInit:createMaterialInit,
         createMaterialBasic:createMaterialBasic,
-        createMaterialPlant:createMaterialPlant
+        createMaterialPlant:createMaterialPlant,
+        createMaterialPurchase:createMaterialPurchase
       }
     },
     components:{
---
+++ src/components/mm/mapper/createMaterial_purchase.js
@@ -0,0 +1,117 @@
+export default {
+    self: this,
+    gridSpan: 12,
+    parameter: {
+        group1: {
+            key: "group1",
+            title: "",
+            type: "formGroupTitle",
+            value: "基本信息"
+        },
+        materialName: {
+            key: "materialName",
+            title: "基本计量单位",
+            type: "input",
+            rule: [{ required: true, message: '请选填写联系人', trigger: 'blur' }]
+        },
+        materialNumber: {
+            key: "materialNumber",
+            title: "订单单位",
+            type: "input",
+            rule: [{ required: true, message: '请选填写联系人', trigger: 'blur' }]
+        },
+        materialType: {
+            key: "materialType",
+            title: "采购组",
+            type: "input",
+            rule: [{ required: true, message: '请选填写联系人', trigger: 'blur' }]
+        },
+        materialGroup: {
+            key: "materialGroup",
+            title: "运输组",
+            type: "input",
+            rule: [{ required: true, message: '请选填写联系人', trigger: 'blur' }]
+        },
+        materialType1: {
+            key: "materialType1",
+            title: "批次管理",
+            type: "input",
+            rule: [{ required: true, message: '请选填写联系人', trigger: 'blur' }]
+        },
+        materialGroup1: {
+            key: "materialGroup1",
+            title: "自动采购",
+            type: "input",
+            rule: [{ required: true, message: '请选填写联系人', trigger: 'blur' }]
+        },
+        group2: {
+            key: "group2",
+            title: "",
+            type: "formGroupTitle",
+            value: "采购数据"
+        },
+        aaa: {
+            key: "aaa",
+            title: "第一封崔询单",
+            type: "input",
+            rule: [{ required: true, message: '请选填写联系人', trigger: 'blur' }]
+
+        },
+        baozhuang1: {
+            key: "baozhuang1",
+            title: "交货不足限度",
+            type: "select",
+            value: [{ key: "canyin", value: "小规模纳税人" }, { key: "ertong", value: "EAN12" }, { key: "tiyu", value: "克" }, { key: "wenchuang", value: "斤" }, { key: "lingshou", value: "两" }],
+            rule: [{ required: true, message: '请选选择行业', trigger: 'change' }]
+        },
+        aaa1: {
+            key: "aaa1",
+            title: "第二封崔询单",
+            type: "input",
+            rule: [{ required: true, message: '请选填写联系人', trigger: 'blur' }]
+
+        },
+        aaa2: {
+            key: "aaa2",
+            title: "过量交货限度",
+            type: "input",
+            rule: [{ required: true, message: '请选填写联系人', trigger: 'blur' }]
+
+        },
+        maozhong: {
+            key: "maozhong",
+            title: "第三封崔询单",
+            type: "input",
+            rule: [{ required: true, message: '请选填写联系人', trigger: 'blur' }]
+        },
+        jingzhong: {
+            key: "jingzhong",
+            title: "最小交货数量",
+            type: "input",
+            rule: [{ required: true, message: '请选填写联系人', trigger: 'blur' }]
+        },
+        jingzhong2: {
+            key: "jingzhong2",
+            title: "请求回执",
+            type: "input",
+            rule: [{ required: true, message: '请选填写联系人', trigger: 'blur' }]
+        },
+        jingzhong3: {
+            key: "jingzhong3",
+            title: "无限交货限制",
+            type: "input",
+            rule: [{ required: true, message: '请选填写联系人', trigger: 'blur' }]
+        },
+    },
+    formValidate: {
+        name: '',
+        logo: [{ 'name': 'a42bdcc1178e62b4694c830f028db5c0', 'url': 'https://o5wwk8baw.qnssl.com/a42bdcc1178e62b4694c830f028db5c0/avatar' }, { 'name': 'bc7521e033abdd1e92222d733590f104', 'url': 'https://o5wwk8baw.qnssl.com/bc7521e033abdd1e92222d733590f104/avatar' }],
+        mail: '',
+        city: '',
+        gender: '',
+        interest: [],
+        date: '',
+        time: '',
+        desc: ''
+    }
+}
